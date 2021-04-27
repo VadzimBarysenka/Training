c51b31bce39e9922a7f7c4a0882fa96a74c112a6
Vadim Borisenko <vdmbrs90@gmail.com>
2021-04-27 10:22:26 +0300

    ArraysTask2

--- src/main/java/ArraysTask.java
+++ src/main/java/ArraysTask.java
@@ -2,7 +2,6 @@ public class ArraysTask {
 
     public static void main(String[] args) {
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-
         int[] num;
         num = new int[5];
         num[0] = 1;
@@ -10,7 +9,6 @@ public class ArraysTask {
         num[2] = 3;
         num[3] = 5;
         num[4] = 8;
-            System.out.println(days.length);
     }
 
 }
