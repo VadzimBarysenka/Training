ffec33820f99fcd2fa0609541bdc2be984bb6052
Vadim Borisenko <vdmbrs90@gmail.com>
2021-05-05 11:28:04 +0300

    input task 2

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -1,10 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter any number:");
-        int num = userInput.nextInt();
-        System.out.println("You've entered: " + num);
+        System.out.println("Enter amount of numbers:");
+        int size = userInput.nextInt();
+        int array[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Enter number #" + (i + 1));
+            array[i] = userInput.nextInt();
+        }
+        System.out.print("You've entered: ");
+        System.out.print(Arrays.toString(array));
+        System.out.println();
     }
 }
