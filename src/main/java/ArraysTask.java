36a9e891e705943c8436060e73744a27f477fd4f
Vadim Borisenko <vdmbrs90@gmail.com>
2021-05-26 21:40:04 +0300

    arr

--- src/main/java/ArraysTask.java
+++ src/main/java/ArraysTask.java
@@ -2,13 +2,13 @@ public class ArraysTask {
 
     public static void main(String[] args) {
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
-        int[] num;
-        num = new int[5];
-        num[0] = 1;
-        num[1] = 2;
-        num[2] = 3;
-        num[3] = 5;
-        num[4] = 8;
+        int[] nums;
+        nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 5;
+        nums[4] = 8;
         int[][] multiNums = {{9, 8, 7, 6}, {5, 4, 2, 1}};
         int[][] multiArr = new int[2][2];
         multiArr[0][0] = 1;
