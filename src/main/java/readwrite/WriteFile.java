7117f071591a6ffa4d453bf11b223523a5a06505
Vadim Borisenko <vdmbrs90@gmail.com>
2021-06-07 18:06:26 +0300

    delbracket

--- src/main/java/readwrite/WriteFile.java
+++ src/main/java/readwrite/WriteFile.java
@@ -6,14 +6,12 @@ public class WriteFile {
 
     public static void writeToFile(String fileName, int[][] array) throws Exception {
         PrintStream output = new PrintStream(fileName);
-        {
-            for (int i = 0; i < array.length; i++) {
-                String s = "";
-                for (int j = 0; j < array[i].length; j++) {
-                    s += array[i][j] + " ";
-                }
-                output.println(s);
+        for (int i = 0; i < array.length; i++) {
+            String s = "";
+            for (int j = 0; j < array[i].length; j++) {
+                s += array[i][j] + " ";
             }
+            output.println(s);
         }
     }
 }
