b138cb935c2f22f40fce40df2f47fc4f02303b02
Vadim Borisenko <vdmbrs90@gmail.com>
2021-06-08 22:33:59 +0300

    readwrite2

--- src/main/java/readwrite/ReadAndWriteTest.java
+++ src/main/java/readwrite/ReadAndWriteTest.java
@@ -2,9 +2,16 @@ package readwrite;
 
 import array.TurnArray;
 
+import java.io.FileNotFoundException;
+
 public class ReadAndWriteTest {
-    public static void main(String args[]) throws Exception {
-        int[][] array = ReadFile.readFromFile("test4");
+    public static void main(String[] args) throws Exception {
+        int[][] array;
+        try {
+            array = ReadFile.readFromFile("test");
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("no file found");
+        }
         int[][] turn = TurnArray.turn(array);
         WriteFile.writeToFile("test2", turn);
     }
