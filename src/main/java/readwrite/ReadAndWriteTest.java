35694dff6a583b21673b0e645e1695f0d92d6881
Vadim Borisenko <vdmbrs90@gmail.com>
2021-06-12 14:16:00 +0300

    mergeraw

--- src/main/java/readwrite/ReadAndWriteTest.java
+++ src/main/java/readwrite/ReadAndWriteTest.java
@@ -2,16 +2,10 @@ package readwrite;
 
 import array.TurnArray;
 
-import java.io.FileNotFoundException;
-
 public class ReadAndWriteTest {
     public static void main(String[] args) throws Exception {
         int[][] array;
-        try {
-            array = ReadFile.readFromFile("test");
-        } catch (FileNotFoundException e) {
-            throw new FileNotFoundException("no file found");
-        }
+        array = ReadFile.readFromFile("test");
         int[][] turn = TurnArray.turn(array);
         WriteFile.writeToFile("test2", turn);
     }
