551bfc8f04f25b4c1cb938740b0d052b5bbede95
Vadim Borisenko <vdmbrs90@gmail.com>
2021-07-15 08:56:14 +0300

    pom_changes

---
+++ src/main/java/fileswork/SortFiles.java
@@ -0,0 +1,17 @@
+package fileswork;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SortAlpha {
+    public static void main(String[] args) throws IOException {
+        final File folder = new File("D:\\test");
+        Files.walk(Paths.get(String.valueOf(folder)))
+                .filter(Files::isRegularFile)
+                .map(Path::getFileName)
+                .sorted()
+                .forEach(System.out::println);
+    }
+}
